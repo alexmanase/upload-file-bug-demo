03b7a226e2de666e8b41adfa9dc7cb44bdf06971
Alex Manase <malexandru699@gmail.com>
2022-06-09 15:31:33 +0300

    chore: remove scripts and styles from Filament

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -10,14 +10,14 @@
         <title>{{ config('app.name') }}</title>
 
         <!-- Styles -->
-        <style>[x-cloak] { display: none !important; }</style>
+        {{-- <style>[x-cloak] { display: none !important; }</style> --}}
         @livewireStyles
-        <link rel="stylesheet" href="{{ mix('css/app.css') }}">
+        {{-- <link rel="stylesheet" href="{{ mix('css/app.css') }}"> --}}
 
         <!-- Scripts -->
         @livewireScripts
-        <script src="{{ mix('js/app.js') }}" defer></script>
-        @stack('scripts')
+        {{-- <script src="{{ mix('js/app.js') }}" defer></script>
+        @stack('scripts') --}}
     </head>
 
     <body class="antialiased">
